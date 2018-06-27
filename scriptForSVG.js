26fb554c6e026ebd41818eb994dbccdfae7bae38
Chi-Shan Yu <chisamyu@seed.net.tw>
2018-06-27 13:28:13 +0800

    Add files via upload

--- scriptForSVG.js
+++ scriptForSVG.js
@@ -1,11 +1,9 @@
 let buildSVGPath = ($polyline, $path, svgHeight) => {
 
       let transformPolylineToPath = (element) => {
-            let points = element
-                        .attr("points")
-                        .replace(/\n/g, " ")
-                        .replace(/  +/g, ' ')
-                        .split(' ');
+            let newElement = {};
+            newElement.debug = 'modified-polyline';
+            let points = element.attr("points").split(' ');
             let path = "M" + points[0];
             for (let i = 1; i < points.length; i++) {
                   if (points[i].includes(",")) {
@@ -18,11 +16,7 @@ let buildSVGPath = ($polyline, $path, svgHeight) => {
       let pointsConverter = (element) => {
             let tmpPoints = new Array();
             let newPoints = new Array();
-            let points = element
-                  .attr("points")
-                  .replace(/\n/g, " ")
-                  .replace(/  +/g, ' ')
-                  .split(' ');
+            let points = element.attr("points").split(' ');
             for (let i = 0; i < points.length; i++) {
                   tmpPoints[i] = (points[i]).split(',');
                   if (tmpPoints[i].length == 2) {
@@ -37,10 +31,10 @@ let buildSVGPath = ($polyline, $path, svgHeight) => {
 
       let path = transformPolylineToPath($polyline);
       let pathPoints = pointsConverter($polyline);
-      let totalLength = 0, sumRatio = 0;
+      let totalLength = 0, sumLength = 0;
       let dX = [],dY = [], distance = [], drawRatio = [];
-      let pathLength = pathPoints.length;
-      for (let i = 1; i < pathLength; i++) {
+      let pointNumber = pathPoints.length;
+      for (let i = 1; i < pointNumber; i++) {
             dX[i] = pathPoints[i].x - pathPoints[i - 1].x;
             dY[i] = pathPoints[i].y - pathPoints[i - 1].y;
             distance[i] = Math.sqrt(dX[i] * dX[i] + dY[i] * dY[i]);
@@ -50,17 +44,22 @@ let buildSVGPath = ($polyline, $path, svgHeight) => {
       $path.attr("stroke-dasharray", totalLength);
       $path.attr("stroke-dashoffset", totalLength);
 
-      for (let i = 1; i < pathLength; i++) {
-            let dRatio = (distance[i] * 5000);
-            let yRatio = (dY[i] / pathPoints[pathLength - 1].y * 5000);
-            let ratio = Math.round(dRatio / yRatio * 100) / 5000;
-            yRatio = Math.round(yRatio);
-            for (let j = 0; j < yRatio; j++) {
-                  sumRatio = sumRatio + ratio;
-                  drawRatio.push(totalLength - sumRatio / 100);
+      // distance: the path length between points
+      // dPath: distance between point(i) and point(i-1)
+      // yPercentage: the percentage of each point scroll position dY in total SVG height
+      // pseudoPointNumber: we break the total points into 5000 points, and the pseudoPoint Number is the point number in the segment between two points 
+      // subLength: the length between each pseudoPoint
+      for (let i = 1; i < pointNumber; i++) {
+            let dPath = distance[i] ;
+            let yPercentage = dY[i] / parseFloat(svgHeight, 10) ;
+            let pseudoPointNumber = Math.round(yPercentage * 5000);
+            let subLength = Math.round(dPath / yPercentage * 100) / 5000 ;
+            for (let j = 0; j < pseudoPointNumber; j++) {
+                  sumLength += subLength;
+                  drawRatio.push(totalLength - sumLength / 100);
             }
       }
-
+      console.log(drawRatio);
       // init controller
       var controller = new ScrollMagic.Controller();
 
